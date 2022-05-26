f642b784b79eb0ce5ad969940cab0e7a1b630949
joye-seo <104261048+joye-seo@users.noreply.github.com>
2022-05-26 11:12:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dailyreport_20220526_retrofit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dailyreport_20220526_retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
